07df90e74f9e02db7a4246a69c366408f0bee71b
Johan Quiroga <johan.c.quiroga@gmail.com>
2018-07-16 09:33:22 -0500

    Completed linear regression for multiple variables and added calculation with normal equation

--- ex1/computeCostMulti.m
+++ ex1/computeCostMulti.m
@@ -6,14 +6,18 @@ function J = computeCostMulti(X, y, theta)
 % Initialize some useful values
 m = length(y); % number of training examples
 
-% You need to return the following variables correctly 
+% You need to return the following variables correctly
 J = 0;
 
 % ====================== YOUR CODE HERE ======================
 % Instructions: Compute the cost of a particular choice of theta
 %               You should set J to the cost.
 
+predictions = X*theta; % predictions of hypothesis on all m examples
 
+sqrErrors = (predictions-y)'*(predictions-y); % squared errors
+
+J = 1/(2*m) * sum(sqrErrors);
 
 
 
--- ex1/gradientDescentMulti.m
+++ ex1/gradientDescentMulti.m
@@ -11,13 +11,15 @@ for iter = 1:num_iters
 
     % ====================== YOUR CODE HERE ======================
     % Instructions: Perform a single gradient step on the parameter vector
-    %               theta. 
+    %               theta.
     %
     % Hint: While debugging, it can be useful to print out the values
     %       of the cost function (computeCostMulti) and gradient here.
     %
 
-
+    predictions = X*theta;
+    delta = (X'*(predictions-y)) / m;
+    theta = theta - alpha * delta;
 
 
 
@@ -29,7 +31,7 @@ for iter = 1:num_iters
 
     % ============================================================
 
-    % Save the cost J in every iteration    
+    % Save the cost J in every iteration
     J_history(iter) = computeCostMulti(X, y, theta);
 
 end
